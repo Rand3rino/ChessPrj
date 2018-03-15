43974245078af1d4685e0d0a50c5bb6c0236d89a
wolverine18 <37334449+wolverine18@users.noreply.github.com>
2018-03-15 19:11:35 -0400

    Edit Knight class

--- src/chess/Knight.java
+++ src/chess/Knight.java
@@ -8,7 +8,21 @@ package chess;
 
 public class Knight extends ChessPiece{
 	
-	public boolean isValidMove() { 
+	protected Knight(Player player) {
+		super(player);
+	}
+
+	public boolean isValidMove(Move move, IChessPiece[][] board) { 
 		return false;
+	}
+
+	@Override
+	public Player player() {
+		return null;
+	}
+
+	@Override
+	public String type() {
+		return "Knight";
 	}
 }
